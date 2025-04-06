4b03a62f8230cdf9e4039f3bf4f8adbdcf78f746
Oeshen Nix <106707318+oeshennix@users.noreply.github.com>
2025-04-05 22:03:30 -0500

    small file changes for grep and added gitignore

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,7 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>fz', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > "), grep_open_files=false})
+  builtin.grep_string({ search = vim.fn.input("Grep > "), grep_open_files=true, search_dirs={vim.cmd.pwd()}})
 end);
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 --vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
